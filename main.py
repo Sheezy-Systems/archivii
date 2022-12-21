1829e0c62a9fa9b73f512ce4dc58e6d0d9b931d3
TheMightyd7 <dd@davisinfo.com>
2022-12-20 20:21:09 -0500

    Add files via upload
    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+import requests
+import bs4
+import re
+import codecs
+import os
+posts = []
+
+class Post:
+    def __init__(self, postID, author, content):
+        self.author = author
+        self.content = content
+        self.postID = postID
+
+    def __str__(self):
+        return f'{self.author}: {self.content}'
+
+def do_request(reqURL, secret):
+    cookies = {
+        'SESS61c75f44be1e14cdb172294ad6a89a4e': secret # Authorization cookie has this name
+    }
+    response = requests.get(reqURL, cookies=cookies)
+    return response
+
+def parseLink(url):
+    response = do_request(url, os.environ["SECRET"])
+    html = json.loads(response.text).get('output')
+    html = re.subn(r'<(script).*?</\1>(?s)', '', html, flags=re.DOTALL)[0]
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    
+    for post in soup.find_all(class_='s-edge-type-update-post'):
+        author = post.find(class_='update-sentence-inner').find('a').text
+        text = ""
+        for i in range(len(post.find_all('p'))):
+            text += post.find_all('p')[i].text + '\n'
+        text = text[:-2]
+        posts.append(Post(None, author, text))
+
+
+    for post in posts:
+        print(str(post) + '\n')
+        
+
+    with codecs.open("out.html", 'w', "utf-8") as f:
+            f.write(str(soup.prettify()))
+
+if __name__ == '__main__':
+    BASE_URL = "https://schoology.tesd.net/group/812485279"
+    url = BASE_URL + '/feed?page=0'
+    parseLink(url)
