329871b423fc39174cc260d98eb1734052607bf6
TheMightyd7 <dd@davisinfo.com>
2022-12-21 22:43:50 -0500

    Add support for comments

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import requests
 import bs4
 import re
 import codecs
+from codecs import encode
 import os
 from dotenv import load_dotenv
+import colorama
 from post import Post
+import chardet
 settings = {}
 posts, tmp = [], []
 previousCount = 0
@@ -15,8 +18,12 @@ def do_request(reqURL):
     cookies = {
         settings.get("COOKIE_NAME"): os.environ["SECRET"] # Authorization cookie has this name
     }
-    response = requests.get(reqURL, cookies=cookies)
-    time.sleep(0.1) # slow down (schoology has a rate limit of 3 requests per second)
+    headers = {
+       "accept": "application/json, text/javascript, */*; q=0.01",
+    }
+    response = requests.get(reqURL, cookies=cookies, headers=headers)
+    response.encoding = 'ascii'
+    time.sleep(0.08) # Schoology has a rate limit of 3 requests per second, this works for some reason
     return response
 
 def parseLink(TYPE, GROUP_ID, page=0, limit=1000):
@@ -25,8 +32,8 @@ def parseLink(TYPE, GROUP_ID, page=0, limit=1000):
     print("Checking page " + str(page) + "...")
     authorID, postID, likeCount, = "" , "", 0
     response = do_request(url)
-    html = json.loads(response.text).get('output') # get html from json
-    html = re.subn(r'(?s)<(script).*?</\1>', '', html, flags=re.DOTALL)[0] # remove js from html
+    html = json.loads(response.text.encode('utf-8').decode("ascii")).get('output') # get html from json
+    html = removeScripts(html)
     soup = bs4.BeautifulSoup(html, 'html.parser') # use bs4 to parse html
     
     #write beautified version
@@ -35,6 +42,7 @@ def parseLink(TYPE, GROUP_ID, page=0, limit=1000):
     
     for post in soup.find_all(class_='s-edge-type-update-post'):
         authorelement = post.find(class_='update-sentence-inner').find('a')
+        commentList = []
         text = ""
         for i in range(len(post.find_all('p'))):
             text += post.find_all('p')[i].text + '\n' # add newline to end of each paragraph
@@ -42,8 +50,6 @@ def parseLink(TYPE, GROUP_ID, page=0, limit=1000):
         for link in post.find_all("a"):
             splitURL = link.get("href").split("/")
             linkClass = link.get("class") # get class name of link
-            # if linkClass == ['like-details-btn'] or "like-details-btn":
-            #     authorID = splitURL[-1]
             if str(link.get("id")).endswith("-show-more-link") and (linkClass == ['show-more-link'] or linkClass == 'show-more-link'): # Has a show more button; all text not being shown
                 text = fetchFullText(splitURL[2], splitURL[-1])
             try:
@@ -54,8 +60,17 @@ def parseLink(TYPE, GROUP_ID, page=0, limit=1000):
                 pass # Not a like count anyways
             text = text.replace("\u00a0", "")
             text = text.replace("\\n", "\n")
+        comments = soup.find_all('div', class_='comment-comment')
+        for comment in comments:
+            if comment.find_parent('div', class_='s-edge-type-update-post') == post:
+                if len(comments) >= 3:
+                    # Schoology only shows 3 comments, so we need to fetch the rest
+                    commentList = fetchAllComments(postID)
+                    break
+                else:
+                    commentList = parseComment(comment)
 
-        tmp.append(Post(authorelement.get("href").split("/")[-1], authorelement.text, postID, likeCount, text))
+        tmp.append(Post(authorelement.get("href").split("/")[-1], authorelement.text, postID, likeCount, text, commentList))
 
     if len(tmp) > previousCount and len(tmp) != 0 and limit > page:
         previousCount = len(tmp)
@@ -67,12 +82,40 @@ def fetchFullText(ID1, ID2):
     url = settings.get("BASE_URL") + "/update_post/" + ID1 + "/show_more/" + ID2
     response = do_request(url)
     try:
-        soup = bs4.BeautifulSoup(json.loads(response.text).get("update"), 'html.parser')
+        soup = bs4.BeautifulSoup(json.loads(response.text).get("update").encode().decode('unicode-escape'), 'html.parser')
     except:
-        print("Error fetching full text")
-        print(response.text)
+        print(colorama.Fore.RED + "Error fetching full text" + colorama.Style.RESET_ALL)
         return ""
     return(soup.text)
+
+def fetchAllComments(postID):
+    url = settings.get("BASE_URL") + "/comment/ajax/" + postID + "&context=updates"
+    response = do_request(url)
+    expression = re.compile("u'2022'", re.UNICODE)
+    decoded = expression.sub("u'u2022'", response.text)
+    return parseComment(removeScripts(decoded))
+
+    
+
+def parseComment(text):
+    commentsList = []
+    parsed = bs4.BeautifulSoup(json.loads(str(text)).get("comments"), 'html.parser')
+    comments = parsed.find_all('div', class_='comment-comment')
+    for comment in comments:
+        author = comment.find(class_='comment-author').find('a')
+        commentText = comment.find(class_='comment-body-wrapper')
+        commentsList.append({
+            "text": commentText.text,
+            "author": {
+                "name": author.text,
+                "id": author.get("href").split("/")[-1]
+            }
+        })
+    print("Found " + str(len(commentsList)) + " comments")
+    return commentsList
+
+def removeScripts(html):
+    return re.subn(r'(?s)<(script).*?</\1>', '', html, flags=re.DOTALL)[0]
 
 if __name__ == '__main__':
     try:
--- post.py
+++ post.py
@@ -1,9 +1,10 @@
 class Post:
-    def __init__(self, authorID, authorName, postID, likeCount, content):
-        self.author = {"Name": authorName, "id": authorID}
+    def __init__(self, authorID, authorName, postID, likeCount, content, comments):
+        self.author = {"name": authorName, "id": authorID}
         self.content = content
         self.likeCount = likeCount
         self.id = postID
+        self.comments = comments
 
     def __str__(self) -> str:
-        return f"{self.author['Name']} ({self.author['id']}) - {self.content} - {self.likeCount} likes"
+        return f"{self.author['name']} ({self.author['id']}) - {self.content} - {self.likeCount} likes"
