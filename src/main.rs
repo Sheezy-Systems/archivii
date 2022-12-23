776e21e8e59c171a21dbeee0c3a89475477def7e
vivi <38266782+humboldt123@users.noreply.github.com>
2022-12-23 12:30:05 -0500

    [feat] abide by rate limit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
-use std::env;
-use std::fmt::format;
+use std::{env, fmt, thread, time::Duration};
+use std::fmt::{format};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -30,14 +30,16 @@ fn main() {
 }
 
 fn make_schoology_request(url: String, secret: &String) -> Response {
-    // TODO: schoology has a limit of 15 requests per five seconds, add a rate limit
     let mut headers = header::HeaderMap::new();
     headers.insert("accept", header::HeaderValue::from_static(ACCEPT));
     headers.insert("cookie", header::HeaderValue::from_str(&*format!("{}={}", AUTH_COOKIE_NAME, secret)).unwrap());
 
     let client = reqwest::blocking::Client::builder().default_headers(headers).build().unwrap();
-
-    client.get(url).send().unwrap()
+    let result = client.get(url).send().unwrap();
+    // schoology has a limit of 15 requests per five seconds
+    // TODO: rewrite this function to instantly send all requests if there are less than 15
+    thread::sleep(Duration::from_millis(334));
+    result
 }
 
 fn parse_link(config:Config, secret: &String) {
@@ -45,18 +47,21 @@ fn parse_link(config:Config, secret: &String) {
     let response: FeedResponse = make_schoology_request(
         format!("{}/{}/{}/feed?page={}", SCHOOLOGY_BASEURL, config.realm, config.id, page),
         secret
-    ).json().expect("Failed to parse response as json or bad SECRET");
+    ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
 
     let soup = Soup::new(&*response.output);
     for post in soup.attr("class", "s-edge-type-update-post").find_all() {
 
+        // Get post ID
         let id = post.attr("class", "like-btn").find().unwrap()
             .get("ajax").unwrap().rsplit("/").next().unwrap().to_string();
 
+        // Get post author
         let author_name = post
             .attr("class", "update-sentence-inner").find().unwrap()
             .tag("a").find().unwrap().text();
 
+        // Get post content
         let mut content: String = post.tag("p").find_all().map(|line| line.text() + "\n").collect();
 
         match post.attr("class", "show-more-link").find() {
@@ -65,7 +70,7 @@ fn parse_link(config:Config, secret: &String) {
                 let response: ShowMoreResponse = make_schoology_request(
                     format!("{}/{}", SCHOOLOGY_BASEURL, show_more.get("href").unwrap()),
                     secret
-                ).json().expect("Failed to parse response as json or bad SECRET");
+                ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
 
                 let soup = Soup::new(&*response.update);
                 content = soup.tag("p").find_all().map(|line| line.text() + "\n").collect();
@@ -74,16 +79,21 @@ fn parse_link(config:Config, secret: &String) {
         }
         content.truncate(content.trim_end_matches(&['\r', '\n'][..]).len());
 
+        // Get post like count
         let like_count: u32 = post.attr("class", "like-details-btn").find().unwrap()
             .text().split(" ").next().unwrap().parse().unwrap();
-        let k = make_schoology_request(
+
+        // Get post comments
+        let mut comment_list: Vec<(String, String)> = Vec::new();
+
+        let comment_response: CommentsResponse = make_schoology_request(
             format!("{}/comment/ajax/{}&context=updates", SCHOOLOGY_BASEURL, id),
             secret
-        );
-        let comment_response: CommentsResponse = k.json().expect("Failed to parse comment response as json or bad SECRET");
-        let mut comment_list: Vec<(String, String)> = Vec::new();
+        ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
+
         let comment_soup = Soup::new(&*comment_response.comments);
         let comments = comment_soup.attr("class", "comment-comment").find_all();
+
         comment_list = comments.map(|comment| {
             let author = comment.attr("class", "comment-author").find().unwrap().tag("a").find().unwrap().text();
             let text = comment.attr("class", "comment-body-wrapper").find().unwrap().text();
@@ -99,23 +109,50 @@ fn parse_link(config:Config, secret: &String) {
 
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
+struct SchoologyPost {
+    author: String,
+    text: String,
+    like_count: u32,
+    likes: Vec<String>,
+    comments: Vec<SchoologyComment>
+}
+
+#[derive(Deserialize)]
+struct SchoologyComment {
+    author: String,
+    text: String,
+    timestamp: String,
+    like_count: u32,
+    likes: Vec<String>
+}
+
+#[derive(Deserialize)]
+struct SchoologyUser {
+    id: String,
+    name: String,
+    timestamp: String,
+    avatar: String
+    // ... todo: badges, email, groups, schools
+}
+
+#[derive(Deserialize)]
 struct FeedResponse {
     output: String
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 struct ShowMoreResponse {
     update: String
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 struct CommentsResponse {
     comments: String,
     count: String // Schoology stores count as a string.
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 struct Config {
     realm: String,
     id: String,
