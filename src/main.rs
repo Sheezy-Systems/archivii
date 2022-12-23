b0883250560735fda90e7fc4f2fc78645e88e85e
vivi <38266782+humboldt123@users.noreply.github.com>
2022-12-23 14:22:26 -0500

    [feat] save output as .json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::{env, fmt, thread, time::Duration};
+use std::{env, fmt, fs, thread, time::Duration};
 use std::collections::HashMap;
 use std::fmt::{format};
 use std::fs::File;
@@ -11,11 +11,11 @@ use reqwest;
 use reqwest::blocking::Response;
 use reqwest::header;
 use soup::prelude::*;
-use tokio::runtime::Handle;
 
 static SCHOOLOGY_BASEURL: &str = "https://schoology.tesd.net";
 static AUTH_COOKIE_NAME: &str = "SESS61c75f44be1e14cdb172294ad6a89a4e";
 static ACCEPT: &str = "application/json, text/javascript, */*; q=0.01";
+static EMPTY_FEEDPAGE: &str = "<div class=\"item-list\"><ul class=\"s-edge-feed feed-no-realm\"><li id=\"feed-empty-message\" class=\"first last\"><div class=\"small gray\">There are no posts</div></li>\n</ul></div>";
 
 fn main() {
     // Make sure our .env file exists and get its SECRET
@@ -26,19 +26,28 @@ fn main() {
     let config_file = BufReader::new(File::open("config.json").expect("Could not find config.json"));
     let config: Config = serde_json::from_reader(config_file).expect("config.json was improperly formatted");
 
+    // Create fresh output directory
+    fs::remove_dir_all("output/").unwrap_or(());
+    fs::create_dir("output/").expect("Failed to create /output directory");
+
     let mut posts: Vec<SchoologyPost> = Vec::new();
     let mut authors: HashMap<String, SchoologyUser> = HashMap::new();
-    for page in (config.start)..(config.start+config.limit) {
+    for page in (config.start)..(config.start + config.limit - 1) {
         let (mut new_posts, new_authors) = scrape_realm_by_page(&config.realm, &config.id, page, &secret);
+        if new_posts.is_empty() {
+            break;
+        }
         posts.append(&mut new_posts);
         authors.extend(new_authors);
     }
-    for (k,v) in authors {
-        println!("{} {}", k, v.name);
-    }
-    for post in posts {
-        println!("==\n{}-{}-{}\n{}\n==", post.author, post.like_count, post.comments.len(), post.content);
-    }
+
+    let realm = SchoologyRealm { posts };
+    let realm_json = serde_json::to_string(&realm).expect("Failed to realm serialize as JSON");
+    fs::write(format!("output/{}_{}.json", &config.realm, &config.id), realm_json).expect("Failed to write to file");
+
+    let users = SchoologyUsers { users: authors };
+    let users_json = serde_json::to_string(&users).expect("Failed to users serialize as JSON");
+    fs::write(format!("output/users.json"), users_json).expect("Failed to write to file");
 }
 
 fn make_schoology_request(url: String, secret: &String) -> Response {
@@ -58,64 +67,23 @@ fn scrape_realm_by_page(realm: &String, id: &String, page: u32, secret: &String)
     let mut posts: Vec<SchoologyPost> = Vec::new();
     let mut authors: HashMap<String, SchoologyUser> = HashMap::new();
 
+    println!("Scanning {} ({}) on page {}...", realm, id, page);
+
     let response: FeedResponse = make_schoology_request(
         format!("{}/{}/{}/feed?page={}", SCHOOLOGY_BASEURL, realm, id, page),
         secret,
     ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
 
-    let soup = Soup::new(&*response.output);
-    for post in soup.attr("class", "s-edge-type-update-post").find_all() {
+    if response.output != EMPTY_FEEDPAGE {
+        let soup = Soup::new(&*response.output);
+        for post in soup.attr("class", "s-edge-type-update-post").find_all() {
 
-        // Get post ID
-        let id = post.attr("class", "like-btn").find().unwrap()
-            .get("ajax").unwrap().rsplit("/").next().unwrap().to_string();
+            // Get post ID
+            let id = post.attr("class", "like-btn").find().unwrap()
+                .get("ajax").unwrap().rsplit("/").next().unwrap().to_string();
 
-        // Get post author
-        let author_anchor = post.attr("class", "update-sentence-inner").find().unwrap().tag("a").find().unwrap();
-        let author_id = author_anchor.get("href").unwrap().rsplit("/").next().unwrap().to_string();
-
-        // if our author isn't in our list, add it
-        if !authors.contains_key(&author_id) {
-            authors.insert(author_id.clone(), SchoologyUser {
-                id: author_id.clone(),
-                name: author_anchor.text(),
-                avatar: "".to_string(), // todo: avatar
-            });
-        }
-
-        // Get post content
-        let mut content: String = post.tag("p").find_all().map(|line| line.text() + "\n").collect();
-
-        match post.attr("class", "show-more-link").find() {
-            // If there's a "Show More" prompt
-            Some(show_more) => {
-                let response: ShowMoreResponse = make_schoology_request(
-                    format!("{}/{}", SCHOOLOGY_BASEURL, show_more.get("href").unwrap()),
-                    secret,
-                ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
-
-                let soup = Soup::new(&*response.update);
-                content = soup.tag("p").find_all().map(|line| line.text() + "\n").collect();
-            }
-            _ => {}
-        }
-
-        content.truncate(content.trim_end_matches(&['\r', '\n'][..]).len());
-
-        let like_count: u32 = post.attr("class", "like-details-btn").find().unwrap()
-            .text().split(" ").next().unwrap().parse().unwrap();
-
-        // Get post comments
-        let mut comments: Vec<SchoologyComment> = Vec::new();
-
-        let comment_response: CommentsResponse = make_schoology_request(
-            format!("{}/comment/ajax/{}&context=updates", SCHOOLOGY_BASEURL, id),
-            secret,
-        ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
-
-        let comment_soup = Soup::new(&*comment_response.comments);
-        comments = comment_soup.attr("class", "comment-comment").find_all().map(|comment| {
-            let author_anchor = comment.attr("class", "comment-author").find().unwrap().tag("a").find().unwrap();
+            // Get post author
+            let author_anchor = post.attr("class", "update-sentence-inner").find().unwrap().tag("a").find().unwrap();
             let author_id = author_anchor.get("href").unwrap().rsplit("/").next().unwrap().to_string();
 
             // if our author isn't in our list, add it
@@ -127,29 +95,84 @@ fn scrape_realm_by_page(realm: &String, id: &String, page: u32, secret: &String)
                 });
             }
 
-            SchoologyComment {
-                author: author_id.clone(),
-                content: comment.attr("class", "comment-body-wrapper").find().unwrap().text(),
-                timestamp: "".to_string(),
-                like_count, // todo: likes and like count
-                likes: vec![],
-            }
-        }).collect();
+            // Get post content
+            let mut content: String = post.tag("p").find_all().map(|line| line.text() + "\n").collect();
 
-        posts.push(SchoologyPost {
-            author: author_id,
-            content,
-            like_count,
-            likes: vec![], // todo: likes
-            comments,
-        })
+            match post.attr("class", "show-more-link").find() {
+                // If there's a "Show More" prompt
+                Some(show_more) => {
+                    let response: ShowMoreResponse = make_schoology_request(
+                        format!("{}/{}", SCHOOLOGY_BASEURL, show_more.get("href").unwrap()),
+                        secret,
+                    ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
+
+                    let soup = Soup::new(&*response.update);
+                    content = soup.tag("p").find_all().map(|line| line.text() + "\n").collect();
+                }
+                _ => {}
+            }
+
+            content.truncate(content.trim_end_matches(&['\r', '\n'][..]).len());
+
+            let like_count: u32 = post.attr("class", "like-details-btn").find().unwrap()
+                .text().split(" ").next().unwrap().parse().unwrap();
+
+            // Get post comments
+            let mut comments: Vec<SchoologyComment> = Vec::new();
+
+            let comment_response: CommentsResponse = make_schoology_request(
+                format!("{}/comment/ajax/{}&context=updates", SCHOOLOGY_BASEURL, id),
+                secret,
+            ).json().expect("Failed to parse response as JSON: Possibly bad SECRET or rate-limiting");
+
+            let comment_soup = Soup::new(&*comment_response.comments);
+            comments = comment_soup.attr("class", "comment-comment").find_all().map(|comment| {
+                let author_anchor = comment.attr("class", "comment-author").find().unwrap().tag("a").find().unwrap();
+                let author_id = author_anchor.get("href").unwrap().rsplit("/").next().unwrap().to_string();
+
+                // if our author isn't in our list, add it
+                if !authors.contains_key(&author_id) {
+                    authors.insert(author_id.clone(), SchoologyUser {
+                        id: author_id.clone(),
+                        name: author_anchor.text(),
+                        avatar: "".to_string(), // todo: avatar
+                    });
+                }
+
+                SchoologyComment {
+                    author: author_id.clone(),
+                    content: comment.attr("class", "comment-body-wrapper").find().unwrap().text(),
+                    timestamp: "".to_string(),
+                    like_count, // todo: likes and like count
+                    likes: vec![],
+                }
+            }).collect();
+
+            posts.push(SchoologyPost {
+                author: author_id,
+                content,
+                like_count,
+                likes: vec![], // todo: likes
+                comments,
+            })
+        }
     }
 
     (posts, authors)
 }
 
+#[derive(Serialize, Deserialize)]
+struct SchoologyUsers {
+    users: HashMap<String, SchoologyUser>
+}
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
+struct SchoologyRealm {
+    // todo: id, name, banner, information, category, members
+    posts: Vec<SchoologyPost>
+}
+
+#[derive(Serialize, Deserialize)]
 struct SchoologyPost {
     author: String,
     content: String,
@@ -158,7 +181,7 @@ struct SchoologyPost {
     comments: Vec<SchoologyComment>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct SchoologyComment {
     author: String,
     content: String,
@@ -167,7 +190,7 @@ struct SchoologyComment {
     likes: Vec<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct SchoologyUser {
     id: String,
     name: String,
@@ -188,7 +211,7 @@ struct ShowMoreResponse {
 #[derive(Deserialize)]
 struct CommentsResponse {
     comments: String,
-    count: String, // Schoology stores count as a string.
+    count: String, // Schoology stores the amount of comments as a string.
 }
 
 #[derive(Deserialize)]
